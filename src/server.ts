c557bc680c63fffaad96c2c6bbb8e3929c31d9f5
Connor Orr <connororr@tutanota.com>
2024-04-17 10:13:45 +1000

    updated host value

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ server.register(routes)
 
 const start = async () => {
   try {
-    await server.listen({ port: Number(fastifyPort) })
+    await server.listen({ port: Number(fastifyPort), host: '0.0.0.0' })
   } catch (err) {
     server.log.error(err)
     process.exit(1)
