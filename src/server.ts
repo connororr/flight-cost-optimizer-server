7ef8da1078d4c655d04afaeac34283866e77abe2
Connor Orr <connororr@tutanota.com>
2024-04-17 12:11:35 +1000

    added header for pre-flight requests

--- src/server.ts
+++ src/server.ts
@@ -20,25 +20,26 @@ await server.register(cors, {
 server.register(routes)
 
 // TODO: remove cors stuff when going to prod
-// server.addHook('preHandler', (req, res, done) => {
-//
-//   const allowedPaths = ['/api/flights', '/api/codes', '*'];
-//   if (allowedPaths.includes(req.routeOptions.url)) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//     res.header("Access-Control-Allow-Headers",  "*");
-//   }
-//
-//   const isPreflight = /options/i.test(req.method);
-//   if (isPreflight) {
-//     return res.send();
-//   }
-//   done();
-// })
+server.addHook('preHandler', (req, res, done) => {
+
+  // const allowedPaths = ['/api/flights', '/api/codes', '*'];
+  // if (allowedPaths.includes(req.routeOptions.url)) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  //   res.header("Access-Control-Allow-Headers",  "*");
+  // }
+
+  const isPreflight = /options/i.test(req.method);
+  if (isPreflight) {
+    res.header('Access-Control-Allow-Origin', 'https://exploria-test.xyz');
+    return res.send();
+  }
+  done();
+})
 
 const start = async () => {
   try {
-    await server.listen({ port: Number(fastifyPort), host: '0.0.0.0' })
+    await server.listen({ port: Number(fastifyPort), host: process.env.HOST || '127.0.0.1' })
   } catch (err) {
     server.log.error(err)
     process.exit(1)
