ea8ec97811b9595549c86fbfa425b2c06d325d28
Connor <connororr@tutanota.com>
2024-05-09 12:01:36 +1000

    removed unused imports and commented out code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import Fastify, { FastifyInstance } from 'fastify'
 import routes from './routes';
-import { Server } from 'tls';
 import cors from '@fastify/cors';
 
 // TODO: remove logging for prod
@@ -9,11 +8,6 @@ const server: FastifyInstance = Fastify({
   logger: true
 })
 
-
-// server.addContentTypeParser('application/json', {}, (req, body, done) => {
-//   done(null, body);
-// });
-
 await server.register(cors, { 
   origin: false
 })
@@ -21,13 +15,6 @@ server.register(routes)
 
 // TODO: remove cors stuff when going to prod
 server.addHook('preHandler', (req, res, done) => {
-
-  // const allowedPaths = ['/api/flights', '/api/codes', '*'];
-  // if (allowedPaths.includes(req.routeOptions.url)) {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  //   res.header("Access-Control-Allow-Headers",  "*");
-  // }
 
   const isPreflight = /options/i.test(req.method);
   if (isPreflight) {
